ef9b12057aefaf64ff0a593411f974ad891107a8
giovanni113 <giovanni.diniz@etec.sp.gov.br>
2019-04-08 20:19:11 -0300

    upload inicial dos arquivos

---
+++ src/Carro/Carro.java
@@ -0,0 +1,35 @@
+package Carro;
+public class Carro { 
+    
+    String nome;
+    String marca;
+    String cor;
+    String fabricacao;       
+    String largura;
+    String peso;
+    
+    public void acelerar() {
+        System.out.println("Carro acelerando...");
+    }
+    
+    public void acionarTracao() {
+        System.out.println("Carro acionando o freio...");
+    }        
+    
+    public void freiar() {
+        System.out.println("Carro freiando...");
+    }
+    
+    public void acionarSeta() {
+        System.out.println("Carro acionando a seta...");
+   
+    }
+    
+          
+
+
+
+
+
+}   
+    
