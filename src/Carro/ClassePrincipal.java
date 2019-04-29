fcf5c662b6101207f6ea1ded59b22b693384e91d
VieiraMidas <guilherme.brasil3@etec.sp.gov.sp>
2019-04-29 19:49:23 -0300

    loja inovada

--- src/Carro/ClassePrincipal.java
+++ src/Carro/ClassePrincipal.java
@@ -10,99 +10,99 @@ public class ClassePrincipal {
         
         Scanner scan = new Scanner(System.in);
         
-        System.out.print("Nome do carro: ");
+        
         carro.nome = scan.next();
         
-        System.out.println("Nome do carro: " + carro.nome);
         
-        System.out.print("Cor do Carro: ");
+        
+        
         carro.cor = scan.next();
         
-        System.out.println("Cor do carro: " + carro.cor);
         
-        System.out.print("Marca do carro: ");
+        
+        
         carro.marca = scan.next();
         
-        System.out.println("Marca do carro: " + carro.marca);
         
-        System.out.print("Ano de fabricação do carro: ");
+        
+        
         carro.fabricacao = scan.next();
         
-        System.out.println("Ano de fabricação do carro: " + carro.fabricacao);
         
-        System.out.print("Largura do Carro: ");
+        
+        
         carro.largura = scan.next();
         
-        System.out.println("Largura do carro: " + carro.largura);
+       
         
-        System.out.print("Peso do carro: ");
+        
         carro.peso = scan.next();
         
-        System.out.println("Peso do carro: " + carro.peso);
+        
         
         Carro carro2 = new Carro();
         
-        System.out.print("Nome do carro: ");
+        
         carro2.nome = scan.next();
         
-        System.out.println("Nome do carro: " + carro2.nome);
         
-        System.out.print("Cor do Carro: ");
+        
+        
         carro2.cor = scan.next();
         
-        System.out.println("Cor do carro: " + carro2.cor);
         
-        System.out.print("Marca do carro: ");
+        
+       
         carro2.marca = scan.next();
         
-        System.out.println("Marca do carro: " + carro2.marca);
         
-        System.out.print("Ano de fabricação do carro: ");
+        
+       
         carro2.fabricacao = scan.next();
         
-        System.out.println("Ano de fabricação do carro: " + carro2.fabricacao);
         
-        System.out.print("Largura do Carro: ");
+        
+       
         carro2.largura = scan.next();
         
-        System.out.println("Largura do carro: " + carro2.largura);
         
-        System.out.print("Peso do carro: ");
+        
+       
         carro2.peso = scan.next();
         
-        System.out.println("Peso do carro: " + carro2.peso);
+       
         
         Carro carro3 = new Carro();
         
-        System.out.print("Nome do carro: ");
+       
         carro3.nome = scan.next();
         
-        System.out.println("Nome do carro: " + carro3.nome);
+     
         
-        System.out.print("Cor do Carro: ");
+      
         carro3.cor = scan.next();
         
-        System.out.println("Cor do carro: " + carro3.cor);
+       
         
-        System.out.print("Marca do carro: ");
+      
         carro3.marca = scan.next();
         
-        System.out.println("Marca do carro: " + carro3.marca);
+      
         
-        System.out.print("Ano de fabricação do carro: ");
+     
         carro3.fabricacao = scan.next();
         
-        System.out.println("Ano de fabricação do carro: " + carro3.fabricacao);
+     
         
-        System.out.print("Largura do Carro: ");
+     
         carro3.largura = scan.next();
         
-        System.out.println("Largura do carro: " + carro3.largura);
+       
         
-        System.out.print("Peso do carro: ");
+     
         carro3.peso = scan.next();
         
-        System.out.println("Peso do carro: " + carro3.peso);
+     
         
         
         
